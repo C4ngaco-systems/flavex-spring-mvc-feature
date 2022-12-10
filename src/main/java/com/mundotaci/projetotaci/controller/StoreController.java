02cbebe334a7c1b3ce5754a93865fc7bbf52b5e4
alexandredornellas <alexandredornellas17@gmail.com>
2022-12-09 22:41:27 -0300

    fix(StoreControler): faltou annotation do controller --'

--- src/main/java/com/mundotaci/projetotaci/controller/StoreController.java
+++ src/main/java/com/mundotaci/projetotaci/controller/StoreController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import com.mundotaci.projetotaci.statics.Estado;
 import com.mundotaci.projetotaci.statics.EstadosEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 @RequestMapping("store")
+@Controller
 public class StoreController {
 
     @Autowired
