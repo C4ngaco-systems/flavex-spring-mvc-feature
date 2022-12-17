36b74cef2570eed27dd78185ce002d83f6291ef4
flavia <flavinha.goncalves@gmail.com>
2022-12-17 01:11:43 -0300

    Ajuste validação cnpj

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -30,3 +30,96 @@ function closeNav()
    document.getElementById("mySidebar").style.width = "0";
    document.getElementById("main").style.marginLeft = "0";
 }
+
+// Validação de dados
+
+function validaCNPJ () {
+
+  var cnpjEl = document.querySelector('.cnpj-input');
+
+  if(!cnpj)return false;
+
+    let cnpj =  parseInt(cnpjEl.value)
+    // Limita ao máximo de 18 caracteres, para CNPJ formatado
+    if (cnpj.length > 18) return false
+
+    // Teste Regex para veificar se é uma string apenas dígitos válida
+    const digitsOnly = /^\d{14}$/.test(cnpj)
+    // Teste Regex para verificar se é uma string formatada válida
+    const validFormat = /^\d{2}.\d{3}.\d{3}\/\d{4}-\d{2}$/.test(cnpj)
+
+    // Se o formato é válido, usa um truque para seguir o fluxo da validação
+
+  if (cnpj.length !== 14) return false
+
+}
+
+function onSubmit(e){
+  console.log('rodou');
+  e.preventDefault();
+  var cnpjEl = document.querySelector('.cnpj-input');
+  if(cnpjEl){
+    console.log(cnpjEl.value, "testeCnpj")
+    if(validarCNPJ(cnpjEl.value)){
+      e.target.submit();
+      return true;
+    }
+  }
+  alert("CNPJ inválido!")
+}
+
+function validarCNPJ(cnpj) {
+ 
+  cnpj = cnpj.replace(/[^\d]+/g,'');
+
+  if(cnpj == '') return false;
+   
+  if (cnpj.length != 14)
+      return false;
+
+  // Elimina CNPJs invalidos conhecidos
+  if (cnpj == "00000000000000" || 
+      cnpj == "11111111111111" || 
+      cnpj == "22222222222222" || 
+      cnpj == "33333333333333" || 
+      cnpj == "44444444444444" || 
+      cnpj == "55555555555555" || 
+      cnpj == "66666666666666" || 
+      cnpj == "77777777777777" || 
+      cnpj == "88888888888888" || 
+      cnpj == "99999999999999")
+      return false;
+       
+  // Valida DVs
+  tamanho = cnpj.length - 2
+  numeros = cnpj.substring(0,tamanho);
+  digitos = cnpj.substring(tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2)
+          pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(0))
+      return false;
+       
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0,tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2)
+          pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(1))
+        return false;
+         
+  return true;
+  
+}
+
+document.getElementById('form-adicionar').addEventListener('submit', onSubmit);
